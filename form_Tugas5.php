98871fe54dd48ded43aec1254b7846454a32f784
Haikal-Rizki-A <haikalrizkiattallah@gmail.com>
2026-03-02 10:59:23 +0700

    Add files via upload

---
+++ form_Tugas5.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Form Belanja Warung </title>
+    </head>
+<body>
+    <h2>Input Data Belanja</h2>
+    <form action="Tugas5.php" method="POST">
+    Memiliki Kartu Member?
+    <input type="radio" name="member" id="1">YA
+    <input type="radio" name="member" id="0">TIDAK
+    <br><br>
+    
+    Nama Pembeli :
+    <input type="text" name="nama"><br><br>
+
+    Nama Barang :
+    <input type="text" name="barang"><br><br>
+
+    Harga Barang :
+    <input type="number" name="harga"><br><br>
+
+    Jumlah Barang :
+    <input type="number" name="jumlah"><br><br>
+
+    <button type="submit">Proses Belanja</button>
+    </form>
+</body>
+</html>
